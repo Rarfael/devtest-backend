c7433c52ac1eb0c567e1527777f3a86a1f36631d
Rafael Godoy <rarfaelgoodoy@gmail.com>
2018-10-30 11:37:01 -0300

    importing softdeletes

--- app/Product.php
+++ app/Product.php
@@ -2,9 +2,14 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['product_name', 'product_type', 'product_description'];
+
+    protected $dates = ['deleted_at'];
 }
