222837998c7b742cc39a62937574828b29d71f13
Rafael Godoy <rarfaelgoodoy@gmail.com>
2018-10-30 13:59:34 -0300

    GET method. Returning all the data

---
+++ app/Http/Resources/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Product extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        //return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'product_name' => $this->product_name,
+            'product_description' => $this->product_description,
+        ];
+    }
+}
